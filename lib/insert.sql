856243d96cba7c19b13d1b701f0630f16d592347
estherkang14 <estherkang14@gmail.com>
2020-06-11 16:08:54 -0400

    created table, inserted values, started querying

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+("Mr. Chocolate", 20, "M", "Brown", "Kind", 1),
+("Rowdy", 8, "M", "Red", "Rowdy", 1),
+("Tabitha", 12, "F", "Brown", "Calm", 1),
+("Sergeant Brown", 18, "F", "Light Brown", "Stern", 1),
+("Melissa", 10, "F", "Red", "Lazy", 1),
+("Grinch", 13, "M", "White", "Mean", 1),
+("Wendy", 3, "F", "Light Brown", "Playful", 1),
+(null, 9, "M", "White", "Evil", 0)
+; 
